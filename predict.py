5b50e43e3bba4ac6f41f0dfd743e6aca7a7b8929
Bill Hill <bill.hill@igmm.ed.ac.uk>
2023-10-10 12:08:43 +0100

    Fix single class evaluation. Add options for CPU only, number of channels, floating point mask output.

--- predict.py
+++ predict.py
@@ -16,21 +16,28 @@ def predict_img(net,
                 full_img,
                 device,
                 scale_factor=1,
+                keep_float=False,
                 out_threshold=0.5):
     net.eval()
     img = torch.from_numpy(BasicDataset.preprocess(None, full_img, scale_factor, is_mask=False))
     img = img.unsqueeze(0)
-    img = img.to(device=device, dtype=torch.float32)
+    img = img.to(device=device, dtype=torch.float)
 
     with torch.no_grad():
         output = net(img).cpu()
         output = F.interpolate(output, (full_img.size[1], full_img.size[0]), mode='bilinear')
-        if net.n_classes > 1:
-            mask = output.argmax(dim=1)
+        mask = None
+        pmask = None
+        if keep_float:
+            mask = torch.sigmoid(output)
+            pmask = mask[0].float().squeeze().numpy()
         else:
-            mask = torch.sigmoid(output) > out_threshold
-
-    return mask[0].long().squeeze().numpy()
+            if net.n_classes > 1:
+                mask = output.argmax(dim=1)
+            else:
+                mask = torch.sigmoid(output) > out_threshold
+            pmask = mask[0].long().squeeze().numpy()
+    return pmask
 
 
 def get_args():
@@ -39,14 +46,19 @@ def get_args():
                         help='Specify the file in which the model is stored')
     parser.add_argument('--input', '-i', metavar='INPUT', nargs='+', help='Filenames of input images', required=True)
     parser.add_argument('--output', '-o', metavar='OUTPUT', nargs='+', help='Filenames of output images')
+    parser.add_argument('--cpu', '-u', action='store_true',
+                        help='Always restrict to CPU')
     parser.add_argument('--viz', '-v', action='store_true',
                         help='Visualize the images as they are processed')
-    parser.add_argument('--no-save', '-n', action='store_true', help='Do not save the output masks')
+    parser.add_argument('--keep-float', '-f', action='store_true',
+                        help='Output mask is floating point probability image.')
+    parser.add_argument('--no-save', '-S', action='store_true', help='Do not save the output masks')
     parser.add_argument('--mask-threshold', '-t', type=float, default=0.5,
                         help='Minimum probability value to consider a mask pixel white')
     parser.add_argument('--scale', '-s', type=float, default=0.5,
                         help='Scale factor for the input images')
     parser.add_argument('--bilinear', action='store_true', default=False, help='Use bilinear upsampling')
+    parser.add_argument('--channels', '-n', type=int, default=3, help='Number of channels (1 greyscale, 3 RGB)')
     parser.add_argument('--classes', '-c', type=int, default=2, help='Number of classes')
     
     return parser.parse_args()
@@ -59,19 +71,25 @@ def get_output_filenames(args):
     return args.output or list(map(_generate_name, args.input))
 
 
-def mask_to_image(mask: np.ndarray, mask_values):
+def mask_to_image(mask: np.ndarray, mask_values, keep_float=False):
     if isinstance(mask_values[0], list):
-        out = np.zeros((mask.shape[-2], mask.shape[-1], len(mask_values[0])), dtype=np.uint8)
+        out = np.zeros((mask.shape[-2], mask.shape[-1], len(mask_values[0])),
+            dtype=np.float if keep_float else np.uint8)
     elif mask_values == [0, 1]:
-        out = np.zeros((mask.shape[-2], mask.shape[-1]), dtype=bool)
+        out = np.zeros((mask.shape[-2], mask.shape[-1]),
+            dtype=np.float if keep_float else bool)
     else:
-        out = np.zeros((mask.shape[-2], mask.shape[-1]), dtype=np.uint8)
+        out = np.zeros((mask.shape[-2], mask.shape[-1]),
+            dtype=np.float if keep_float else np.uint8)
 
     if mask.ndim == 3:
         mask = np.argmax(mask, axis=0)
 
-    for i, v in enumerate(mask_values):
-        out[mask == i] = v
+    if keep_float:
+        out[:] = mask.astype(np.float16)
+    else:
+        for i, v in enumerate(mask_values):
+            out[mask == i] = v
 
     return Image.fromarray(out)
 
@@ -83,9 +101,9 @@ if __name__ == '__main__':
     in_files = args.input
     out_files = get_output_filenames(args)
 
-    net = UNet(n_channels=3, n_classes=args.classes, bilinear=args.bilinear)
+    net = UNet(n_channels=args.channels, n_classes=args.classes, bilinear=args.bilinear)
 
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    device = torch.device('cuda' if (not args.cpu) and torch.cuda.is_available() else 'cpu')
     logging.info(f'Loading model {args.model}')
     logging.info(f'Using device {device}')
 
@@ -103,12 +121,13 @@ if __name__ == '__main__':
         mask = predict_img(net=net,
                            full_img=img,
                            scale_factor=args.scale,
+                           keep_float=args.keep_float,
                            out_threshold=args.mask_threshold,
                            device=device)
 
         if not args.no_save:
             out_filename = out_files[i]
-            result = mask_to_image(mask, mask_values)
+            result = mask_to_image(mask, mask_values, keep_float=args.keep_float)
             result.save(out_filename)
             logging.info(f'Mask saved to {out_filename}')
 
